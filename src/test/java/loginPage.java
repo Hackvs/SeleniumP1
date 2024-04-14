cb1bf4befa77a7f8469d2ca0e750c80b1c1795ab
Hackvs <vishus2112@gmail.com>
2024-04-15 00:47:49 +0530

    Second commit and push

--- src/test/java/loginPage.java
+++ src/test/java/loginPage.java
@@ -7,6 +7,9 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.Calendar;
+import java.util.List;
+
 import static java.lang.Thread.sleep;
 
 
@@ -35,7 +38,7 @@ public class loginPage {
 
     @Test
     void makingAnAppointment() throws InterruptedException {
-        WebElement dropDownFacility =  driver.findElement(By.xpath("/html/body/section/div/div/form/div[1]/div/select"));
+        WebElement dropDownFacility =  By.xpath("/html/body/section/div/div/form/div[1]/div/select").findElement(driver);
         if (dropDownFacility.getTagName().equalsIgnoreCase("select")) {
             Select facility = new Select(dropDownFacility);
             facility.selectByIndex(1);
@@ -43,6 +46,72 @@ public class loginPage {
         driver.findElement(By.xpath("/html/body/section/div/div/form/div[2]/div/label/input")).click();
         driver.findElement(By.xpath("/html/body/section/div/div/form/div[3]/div/label[2]/input")).click();
         driver.findElement(By.xpath("/html/body/section/div/div/form/div[4]/div/div/input"));
+
+        String date = "23/06/2025";
+
+        String[] date_dd_MM_yyyy = (date.split(" ")[0]).split("/");
+
+        int yearDiff = Integer.parseInt(date_dd_MM_yyyy[2])- Calendar.getInstance().get(Calendar.YEAR);
+        //For selecting date
+        WebElement date_input = driver.findElement(By.xpath("/html/body/section/div/div/form/div[4]/div/div/input"));
+        date_input.click();
+        //button for mid link
+        WebElement midLink = driver.findElement(By.xpath("/html/body/div[2]/div[1]/table/thead/tr[2]/th[2]"));
+
+        //button for next year
+        WebElement nextYear = driver.findElement(By.xpath("/html/body/div[2]/div[1]/table/thead/tr[2]/th[3]"));
+
+        //button for next year
+        WebElement prevYear = driver.findElement(By.xpath("/html/body/div[2]/div[1]/table/thead/tr[2]/th[1]"));
+
+        midLink.click();
+        if(yearDiff!=0){
+
+            //if you have to move next year
+
+            if(yearDiff>0){
+
+                for(int i=0;i< yearDiff;i++){
+
+                    System.out.println("Year Diff->"+i);
+
+                    nextYear.click();
+
+                }
+
+            }
+
+            //if you have to move previous year
+
+            else {
+
+                for(int i=0;i< (yearDiff*(-1));i++){
+
+                    System.out.println("Year Diff->"+i);
+
+                    prevYear.click();
+
+                }
+
+            }
+
+        }
+
+        Thread.sleep(1000);
+
+        //Get all months from calendar to select correct one
+        List<WebElement> list_AllMonthToBook = driver.findElements(By.xpath("/html/body/div[2]/div[2]/table/thead/tr[2]/th[2]"));
+
+        list_AllMonthToBook.get(Integer.parseInt(date_dd_MM_yyyy[1])-1).click();
+
+        Thread.sleep(1000);
+
+        //get all dates from calendar to select correct one
+
+        List<WebElement> list_AllDateToBook = driver.findElements(By.xpath("/html/body/div[2]/div[2]/table/tbody/tr/td/span[8]"));
+
+        list_AllDateToBook.get(Integer.parseInt(date_dd_MM_yyyy[0])-1).click();
+
 
     }
 
