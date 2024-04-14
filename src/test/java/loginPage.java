ffbed8ceaa385764de17563939525da4082c2d0e
Hackvs <vishus2112@gmail.com>
2024-04-14 18:51:01 +0530

    Second commit and push

--- src/test/java/loginPage.java
+++ src/test/java/loginPage.java
@@ -1,2 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class loginPage {
+    @Test
+    void setUp(){
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        String url = "https://katalon-demo-cura.herokuapp.com/";
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
+
 }
