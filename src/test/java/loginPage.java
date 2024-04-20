f750e974b2d341efcab81b395e73883b62993eb8
Hackvs <vishus2112@gmail.com>
2024-04-20 21:53:06 +0530

    latest commit and push

--- src/test/java/loginPage.java
+++ src/test/java/loginPage.java
@@ -3,12 +3,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.Calendar;
-import java.util.List;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static java.lang.Thread.sleep;
 
@@ -26,7 +28,7 @@ public class loginPage {
         driver.get(url);
 
     }
-    @Test
+    @Test (priority = 0)
     void login() throws InterruptedException {
         driver.findElement(By.id("btn-make-appointment")).click();
         driver.findElement(By.id("txt-username")).sendKeys("John Doe");
@@ -36,85 +38,49 @@ public class loginPage {
         System.out.println("Login successful");
     }
 
-    @Test
+    @Test (priority = 1)
     void makingAnAppointment() throws InterruptedException {
         WebElement dropDownFacility =  By.xpath("/html/body/section/div/div/form/div[1]/div/select").findElement(driver);
         if (dropDownFacility.getTagName().equalsIgnoreCase("select")) {
             Select facility = new Select(dropDownFacility);
             facility.selectByIndex(1);
         }
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        String date1 = dateFormat.format(date);
         driver.findElement(By.xpath("/html/body/section/div/div/form/div[2]/div/label/input")).click();
         driver.findElement(By.xpath("/html/body/section/div/div/form/div[3]/div/label[2]/input")).click();
-        driver.findElement(By.xpath("/html/body/section/div/div/form/div[4]/div/div/input"));
+        driver.findElement(By.xpath("/html/body/section/div/div/form/div[4]/div/div/input")).sendKeys(date1);
+        Thread.sleep(50);
+        driver.findElement(By.xpath("//textarea[@id='txt_comment']")).sendKeys("Book an appointment for my uncle. " +
+                "Thanks!!");
+        driver.findElement(By.xpath("//button[@id='btn-book-appointment']")).click();
+        Thread.sleep(50);
 
-        String date = "23/06/2025";
-
-        String[] date_dd_MM_yyyy = (date.split(" ")[0]).split("/");
-
-        int yearDiff = Integer.parseInt(date_dd_MM_yyyy[2])- Calendar.getInstance().get(Calendar.YEAR);
-        //For selecting date
-        WebElement date_input = driver.findElement(By.xpath("/html/body/section/div/div/form/div[4]/div/div/input"));
-        date_input.click();
-        //button for mid link
-        WebElement midLink = driver.findElement(By.xpath("/html/body/div[2]/div[1]/table/thead/tr[2]/th[2]"));
-
-        //button for next year
-        WebElement nextYear = driver.findElement(By.xpath("/html/body/div[2]/div[1]/table/thead/tr[2]/th[3]"));
-
-        //button for next year
-        WebElement prevYear = driver.findElement(By.xpath("/html/body/div[2]/div[1]/table/thead/tr[2]/th[1]"));
-
-        midLink.click();
-        if(yearDiff!=0){
-
-            //if you have to move next year
-
-            if(yearDiff>0){
-
-                for(int i=0;i< yearDiff;i++){
-
-                    System.out.println("Year Diff->"+i);
-
-                    nextYear.click();
-
-                }
-
-            }
-
-            //if you have to move previous year
-
-            else {
-
-                for(int i=0;i< (yearDiff*(-1));i++){
-
-                    System.out.println("Year Diff->"+i);
-
-                    prevYear.click();
-
-                }
-
-            }
-
-        }
-
-        Thread.sleep(1000);
-
-        //Get all months from calendar to select correct one
-        List<WebElement> list_AllMonthToBook = driver.findElements(By.xpath("/html/body/div[2]/div[2]/table/thead/tr[2]/th[2]"));
-
-        list_AllMonthToBook.get(Integer.parseInt(date_dd_MM_yyyy[1])-1).click();
-
-        Thread.sleep(1000);
-
-        //get all dates from calendar to select correct one
-
-        List<WebElement> list_AllDateToBook = driver.findElements(By.xpath("/html/body/div[2]/div[2]/table/tbody/tr/td/span[8]"));
-
-        list_AllDateToBook.get(Integer.parseInt(date_dd_MM_yyyy[0])-1).click();
-
-
+        String facility = driver.findElement(By.xpath("//p[@id='facility']")).getText();
+        String hospitalReadmission  = driver.findElement(By.xpath("//p[@id='hospital_readmission']")).getText();
+        String healthcareProgram = driver.findElement(By.xpath("//p[@id='program']")).getText();
+        String visitDate = driver.findElement(By.xpath("//p[@id='visit_date']")).getText();
+        String comment = driver.findElement(By.xpath("//p[@id='comment']")).getText();
+        System.out.println("------------Appointment Information-------------");
+        System.out.println("Facility : "+facility);
+        System.out.println("Apply for hospital readmission : "+hospitalReadmission);
+        System.out.println("Healthcare Program : "+healthcareProgram);
+        System.out.println("Visit Date : "+visitDate);
+        System.out.println("Comment : "+comment);
+        System.out.println("------------------------------------------------");
+        Thread.sleep(100);
+        driver.findElement(By.cssSelector(".btn.btn-default")).click();
+        Thread.sleep(100);
     }
 
+    @Test (priority = 2)
+    void logOut() throws InterruptedException {
+        driver.findElement(By.xpath("//a[@id='menu-toggle']")).click();
+        driver.findElement(By.xpath("//a[@href='authenticate.php?logout']")).click();
+        Thread.sleep(50);
+        System.out.println("Logout Successful!!!");
+    }
     @AfterTest
     void tearDown(){
         driver.close();
